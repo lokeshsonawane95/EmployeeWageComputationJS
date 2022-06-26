b70a887d6880767e1adcfc4f2f12ee1076f784b1
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-06-26 15:59:59 +0530

    Added to main

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,4 +1,4 @@
-//UC4
+//UC5
 const IS_PART_TIME = 1
 const IS_FULL_TIME = 2
 const PART_TIME_HOURS = 4
@@ -14,11 +14,14 @@ function getWorkingHours(empCheck){
             return 0
     }
 }
-const NUM_OF_WORKING_DAYS = 2
-let empHrs = 0
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+const MAX_HRS_IN_MONTH = 100
+const NUM_OF_WORKING_DAYS = 10
+let totalEmpHrs = 0
+let totalWorkingDays = 0
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS){
+    totalWorkingDays++
     let empCheck = Math.floor(Math.random() * 10) % 3
-    empHrs += getWorkingHours(empCheck)
+    totalEmpHrs += getWorkingHours(empCheck)
 }
-let empWage = empHrs * WAGE_PER_HOUR
-console.log("Employee Wage : " + empWage)
+let empWage = totalEmpHrs * WAGE_PER_HOUR
+console.log("Total Days : " + totalWorkingDays + " Total Hours : " + totalEmpHrs + " Employee Wage : " + empWage)
