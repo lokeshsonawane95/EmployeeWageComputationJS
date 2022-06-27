30eeb7ee404d243a354481bb63c691f8b8056468
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-06-27 18:33:46 +0530

    Added to DailyWageAlongWithTotalWage

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -14,14 +14,20 @@ function getWorkingHours(empCheck){
             return 0
     }
 }
-const MAX_HRS_IN_MONTH = 100
-const NUM_OF_WORKING_DAYS = 10
+function calcDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR
+}
+const MAX_HRS_IN_MONTH = 160
+const NUM_OF_WORKING_DAYS = 20
 let totalEmpHrs = 0
 let totalWorkingDays = 0
-while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS){
+let empDailyWageArr = new Array()
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
     totalWorkingDays++
     let empCheck = Math.floor(Math.random() * 10) % 3
-    totalEmpHrs += getWorkingHours(empCheck)
+    let empHrs = getWorkingHours(empCheck)
+    totalEmpHrs += empHrs
+    empDailyWageArr.push(calcDailyWage(empHrs))
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR
+let empWage = calcDailyWage(totalEmpHrs)
 console.log("Total Days : " + totalWorkingDays + " Total Hours : " + totalEmpHrs + " Employee Wage : " + empWage)
