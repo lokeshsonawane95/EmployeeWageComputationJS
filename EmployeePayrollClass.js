7e5b0159d0306c2a4837f950e3be1735731d7318
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-06-28 20:58:20 +0530

    Added to RegexAndTryCatchForOthers

--- EmployeePayrollClass.js
+++ EmployeePayrollClass.js
@@ -1,8 +1,8 @@
 class EmployeePayrollData{
-    id
-    salary
-    gender
-    startDate
+    // id
+    // salary
+    // gender
+    // startDate
 
     constructor(...params){
         this.id = params[0]
@@ -21,6 +21,33 @@ class EmployeePayrollData{
         else throw "Name is in incorrect format"
     }
 
+    get id() { return this._id }
+    set id(id){
+        let idRegex = RegExp('^[1-9]+$')
+        if(idRegex.test(id)){
+            this._id = id
+        }
+        else throw "id is not in correct format"
+    }
+
+    get gender() { return this._gender }
+    set gender(gen){
+        let genderRegex = RegExp('^(M|F)$')
+        if(genderRegex.test(gen)){
+            this._gender = gen
+        }
+        else throw "Gender is not in correct format"
+    }
+
+    get startDate() { return this._startDate }
+    set startDate(startDate){
+        let currentDate = new Date()
+        if(currentDate - startDate >= 0){
+            this._startDate = startDate
+        }
+        else throw "startDate is not correct"
+    }
+
     toString(){
         const options = { year: 'numeric', month: 'long', day: 'numeric' }
         const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options)
@@ -28,14 +55,15 @@ class EmployeePayrollData{
         ", startDate : " + empDate 
     }
 }
-let employeePayrollData = new EmployeePayrollData(1, "Lokesh", 30000)
+let employeePayrollData = new EmployeePayrollData(1, "Lokesh", 30000, "M", new Date())
 console.log(employeePayrollData.toString())
 try{
-    employeePayrollData.name = "Loki"
+    employeePayrollData.id = 4
+    employeePayrollData.name = "Mani";
+    employeePayrollData.salary = 45000
+    employeePayrollData.gender = "k"
 }
 catch(e){
     console.error(e)
 }
 console.log(employeePayrollData.toString())
-let newEmployeePayrollData = new EmployeePayrollData(1, "Lokesh", 30000, "M", new Date())
-console.log(newEmployeePayrollData.toString())
